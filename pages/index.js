45a527ff874849871d43042cf5dc950bd0dedbd8
Vinay Bhinde <vinay@avestatechnologies.com>
2022-09-04 09:16:54 +0530

    feat: setup repo for workshop

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import { useEffect, useState } from 'react'
 
 export async function getServerSideProps({ params, req, res, query, preview, previewData, resolvedUrl, locale, locales, defaultLocale }) {
-  console.log('Logging : ' + res);
-  const data = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await data.json();
-  return { props: { users } }
+  /*
+  server side code
+    tasks
+      - fetch users data server side
+  */
+
 }
 
 export default function Home({ users }) {
   const [reactData, setReactData] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => {
-        setReactData(data);
-      }).catch((e) => { console.log(e) });
-  }, []);
+  /*
+  client side code
+    tasks
+      - fetch same data client side
+      - show user detail in alert
+  */
 
   return (
     <>
@@ -42,13 +43,10 @@ export default function Home({ users }) {
           <tr key={"hello1"}>
             <th colSpan='3' className='topnav'>Rendered By Next JS | Server side rendered</th>
           </tr>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td><a href={`/user/${user.id}`}>{user.name}</a></td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
+         {/*
+         todo
+            - 1 row for each users detail
+          */}
         </tbody>
       </table>
     </>
--- pages/user/[userId].js
+++ pages/user/[userId].js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react'
 
+
 export async function getServerSideProps({ params, req, res, query, preview, previewData, resolvedUrl, locale, locales, defaultLocale }) {
-    console.log('Logging : ' + JSON.stringify(params));
-    const userId = params.userId
-    console.log(`UserId: ${userId}`);
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    const userDetail = await data.json();
-    console.log(userDetail)
-    return { props: { userDetail } }
+
+    /**
+     * Server side code
+     *  tasks
+     *      - fetch details for user by user id 
+     *      - rest endpoint https://jsonplaceholder.typicode.com/users/${userId}
+     */
+    
 }
 
 export default function Home({ userDetail }) {
@@ -16,16 +18,11 @@ export default function Home({ userDetail }) {
             <h1>SSR Workshop | Next.js Exercise</h1>
             <h2>User detail page</h2>
             <br />
-            <table>
-                <tbody>
-                    <tr>
-                        <th colSpan='3' className='topnav'>Rendered By Next JS | Server side rendered</th>
-                    </tr>
-                    <tr><span>Name:{userDetail.name}</span></tr>
-                    <tr><span>Email:{userDetail.email}</span></tr>
-                    <tr><span>Address :{`${userDetail.address.street} ${userDetail.address.suite} ${userDetail.address.city}`}</span></tr>
-                </tbody>
-            </table>
+            {/**
+             * Client side code
+             *  todo
+             *     - HTML to render details of user
+             */}
         </>
     )
 }
